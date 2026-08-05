c4d52625c435f6249167018f4344c3b25eca404f
Sandeep Nigam <nigamsandeep876@gmail.com>
2026-08-05 22:54:56 +0530

    File reference and App init file added

---
+++ Travel App/TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Sandeep Nigam on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
