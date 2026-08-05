7dff7679ead6ba5d288ac7865359dc8e8edcd1fd
Sandeep Nigam <nigamsandeep876@gmail.com>
2026-08-05 22:55:31 +0530

    Test case file added

---
+++ Travel App/TravelApp/TravelAppTests/TravelAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppTests.swift
+//  TravelAppTests
+//
+//  Created by Sandeep Nigam on 11/05/26.
+//
+
+import Testing
+@testable import TravelApp
+
+struct TravelAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
